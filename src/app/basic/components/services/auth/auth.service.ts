3de6ae8c9ddd1611e1e38d0bbec6a4d3d203072b
Serges YAMAKO <yamakoserges@gmail.com>
2024-04-16 00:59:16 +0100

    update

--- src/app/basic/components/services/auth/auth.service.ts
+++ src/app/basic/components/services/auth/auth.service.ts
@@ -23,9 +23,9 @@ export class AuthService {
   }
 
   login(username:string, password:string){
-    return this.http.post(BASIC_URL + "authenticate", {username, password}, {observe:'response'})
-    pipe(
-      map((res:HttpResponse<any>)=>{
+    return this.http.post(BASIC_URL + "authenticate", {username, password}, { observe:'response' })
+    .pipe(
+      map((res: HttpResponse<any>) =>{
         console.log(res.body)
         const tokenLength = res.headers.get(AUTH_HEADER)?.length;
         const bearerToken = res.headers.get(AUTH_HEADER)?.substring(7, tokenLength);
